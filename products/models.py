771afef650a7325c792bb9367a861998a601bb01
jyouung90ie <jyoung90ie@gmail.com>
2020-03-12 15:49:07 +0700

    Updates product model to use uuid
    Product model updated to replace auto-incrementing
    id field with uuid for a more
    secure application.
    
    get_absolute_url defined for product model.
    
    Urls.py updated to expect uuid instead of int argument.
    
    Old Product migrations removed and replaced with new migrations using uuid
    for id field.

--- products/models.py
+++ products/models.py
@@ -1,8 +1,11 @@
+import uuid
 from django.db import models
+from django.urls import reverse
 
 
 class Product(models.Model):
     """Store product model"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     brand = models.CharField(max_length=200)
     category = models.CharField(max_length=100)
@@ -13,4 +16,9 @@ class Product(models.Model):
     is_live = models.BooleanField(default=True)
 
     def __str__(self):
+        """return product title by default"""
         return self.title
+
+    def get_absolute_url(self):
+        """define default url for an instance of product model"""
+        return reverse('product_detail', args=[str(self.id)])
