db247198b663eab7b5982a81bf6d408c5400cb01
jyouung90ie <jyoung90ie@gmail.com>
2020-03-13 21:47:24 +0700

    Adds create, update & delete functionality
    Views, URLs, and templates added to allow end-users to perform the full
     CRUD functionality.

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ProductCreateView, \
+    ProductUpdateView, ProductDeleteView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
     path('<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('<uuid:pk>/update/', ProductUpdateView.as_view(),
+         name='product_update'),
+    path('<uuid:pk>/delete/', ProductDeleteView.as_view(),
+         name='product_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, \
+    UpdateView, DeleteView
 
 from .models import Product
 
@@ -15,3 +18,29 @@ class ProductDetailView(DetailView):
     """Render output for a single product which is deemed to be live"""
     queryset = Product.objects.filter(is_live=True)
     template_name = 'products/product_detail.html'
+
+
+class ProductCreateView(PermissionRequiredMixin, CreateView):
+    """Authorized users can add new products"""
+    permission_required = 'products.add_product'
+    model = Product
+    fields = '__all__'
+    template_name = 'products/product_create.html'
+
+
+class ProductUpdateView(PermissionRequiredMixin, UpdateView):
+    """Authorized users can update all product fields"""
+    permission_required = 'products.change_product'
+    model = Product
+    fields = '__all__'
+    context_object_name = 'product'
+    template_name = 'products/product_update.html'
+
+
+class ProductDeleteView(PermissionRequiredMixin, DeleteView):
+    """Authorized users can delete products"""
+    permission_required = 'products.delete_product'
+    model = Product
+    context_object_name = 'product'
+    template_name = 'products/product_delete.html'
+    success_url = reverse_lazy('product_list')
