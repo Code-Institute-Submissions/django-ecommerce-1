960c65c2e4d1f78d80be808462a76ccb2f8caf53
jyouung90ie <jyoung90ie@gmail.com>
2020-04-01 21:05:26 +0100

    Adds humanize app

--- settings/base.py
+++ settings/base.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.humanize',
 
     # third party
     'crispy_forms',
