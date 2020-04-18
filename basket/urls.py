08de2ff2e2cae1077ee73a7c52fdf74866a09be4
jyouung90ie <jyoung90ie@gmail.com>
2020-04-18 18:53:15 +0100

    Removes unnecessary urls and renames product

--- basket/urls.py
+++ basket/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 
-from .views import add_to_basket, view_basket, remove_from_basket, \
-    update_basket
+from .views import add_to_basket, view_basket
 
 urlpatterns = [
     path('', view_basket, name='basket'),
-    path('add/<uuid:product>/', add_to_basket, name='add_to_basket'),
-    path('remove/<uuid:product>/', remove_from_basket,
-         name='remove_from_basket'),
-    path('update/', update_basket, name='update_basket'),
+    path('add/<uuid:product_id>/', add_to_basket, name='add_to_basket'),
 ]
