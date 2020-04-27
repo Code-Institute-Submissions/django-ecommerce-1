f2da70bb394125cc78b3871f84aa2c2de17093e4
jyouung90ie <jyoung90ie@gmail.com>
2020-04-27 21:20:38 +0100

    Adds carousel code and moves stripe initialisation
    Carousel code added to enable the display of multiple rotating product
    views on the homepage. This JS has been heavily modified by me to permit
    multiple carousels as opposed to just one.

--- static/js/payment.js
+++ static/js/payment.js
@@ -1,4 +1,7 @@
-// Create an instance of Elements.
+// initialise stripe functionality
+var stripe = Stripe('pk_test_j8f56cTAACEJUymJMZftX16500A9hjRgzB');
+
+// initialise elements
 var elements = stripe.elements();
 var card = elements.create('card');
 
