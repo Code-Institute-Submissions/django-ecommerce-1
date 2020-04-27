bdeb14004d25727ffaaea8930829f05af1866de3
jyouung90ie <jyoung90ie@gmail.com>
2020-04-27 21:04:24 +0100

    Incorporates new functionality
    Else clause added to handle when user's log-in with no session variable,
    for example, on a new device. This new logic will check if the user has
    a basket and update the request object.

--- basket/middleware.py
+++ basket/middleware.py
@@ -11,17 +11,26 @@ class BasketMiddleware:
     def __call__(self, request):
         # check that a basket session exists
         basket_id = request.session.get('basket_id', False)
-        if basket_id:
-            # make sure the basket exists in the db
-            try:
-                # store basket object in request object to access from any view
+
+        try:
+            if basket_id:
+                # if session variable exists, store basket object in request
+                # object to access from any view
                 request.basket = Basket.objects.get(
                     id=basket_id,
                     status=Basket.IN_PROGRESS)
-            except Basket.DoesNotExist:
-                # basket not in db, unset variables
-                request.session['basket_id'] = None
-                request.basket = None
+            else:
+                # make sure user is logged in
+                if request.user.is_authenticated:
+                    # no session variable, check if user has basket in db
+                    # store in request object
+                    request.basket = Basket.objects.get(
+                        user=request.user,
+                        status=Basket.IN_PROGRESS)
+        except Basket.DoesNotExist:
+            # basket not in db, unset variables
+            request.session['basket_id'] = None
+            request.basket = None
 
         response = self.get_response(request)
 
