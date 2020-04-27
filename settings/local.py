52665aaebdf5db68d8f9cad462789090467e2f7d
jyouung90ie <jyoung90ie@gmail.com>
2020-04-27 21:18:24 +0100

    Updates ALLOWED_HOSTS to enable network testing

--- settings/local.py
+++ settings/local.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.11', ]
 
 DATABASES = {
     'default': {
