00c2f12faba80a16d2f01c4d430cbfbc69e2ed4c
jyouung90ie <jyoung90ie@gmail.com>
2020-04-27 21:17:41 +0100

    Adds order history view
    The order history view displays all a user's orders, the status, and
    a breakdown of the individual items within the order. This gives the
    user visibility on the status of their orders.

--- orders/urls.py
+++ orders/urls.py
@@ -1,1 +1,7 @@
-# from django.urls import path
+from django.urls import path
+
+from .views import OrderHistoryView
+
+urlpatterns = [
+    path('history/', OrderHistoryView.as_view(), name='order_history')
+]
--- orders/views.py
+++ orders/views.py
@@ -1,1 +1,13 @@
-# from django.shortcuts import render
+from django.views.generic.list import ListView
+from checkout.models import Order
+
+
+class OrderHistoryView(ListView):
+    """Show user's orders"""
+    paginate_by = 10
+    template_name = 'orders/order_history.html'
+
+    def get_queryset(self):
+        user_orders = Order.objects.filter(
+            user=self.request.user).order_by('-order_date', 'id')
+        return user_orders
