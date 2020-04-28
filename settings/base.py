3197378e8d51268e9c2683cc03d1c2abce07ac44
jyouung90ie <jyoung90ie@gmail.com>
2020-04-28 18:56:59 +0100

    Adds packages and settings for deployment
    Whitenoise is used for handling staticfiles in deployment.
    GUnicorn is the webserver used.
    Dj-database-url is used to connect to a remote PostgreSQL DB via URL.

--- settings/base.py
+++ settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',  # does not use whitenoise on local server
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.humanize',
@@ -50,12 +51,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # user middleware
     'basket.middleware.BasketMiddleware',
 ]
 
---
+++ settings/production.py
@@ -0,0 +1,17 @@
+import os
+
+import dj_database_url
+
+from .base import *
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {}
+DATABASES['default'].update(db_from_env)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
