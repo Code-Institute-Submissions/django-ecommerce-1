27db76a8777fd47bddb35b4974ade452a32982ec
jyouung90ie <jyoung90ie@gmail.com>
2020-04-28 19:39:59 +0100

    Amends empty dictionary format

--- settings/production.py
+++ settings/production.py
@@ -5,7 +5,10 @@ import dj_database_url
 from .base import *
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES = {}
+
+DATABASES = {
+    'default': {}
+}
 DATABASES['default'].update(db_from_env)
 
 # SECURITY WARNING: keep the secret key used in production secret!
