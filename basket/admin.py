b1c9aef89e2aa497272f5a08a7731d0382524f84
jyouung90ie <jyoung90ie@gmail.com>
2020-04-30 18:39:21 +0100

    Updates admin area for Basket model
    List view is now functional, detailing baskets by user's (or
    anonymous), including status and item count.
    
    Admin can now modify basket status in list view, this makes testing
    easier.

--- basket/admin.py
+++ basket/admin.py
@@ -9,9 +9,31 @@ class BasketItemInline(admin.TabularInline):
 
 
 class BasketAdmin(admin.ModelAdmin):
+    list_display = ('account_identifier', 'status', 'item_count')
+    list_display_links = ('account_identifier', )
+    list_editable = ('status', )
+    list_filter = ('status', )
+
     inlines = [
         BasketItemInline,
     ]
+
+    def account_identifier(self, obj):
+        """ Return user field """
+        if obj.user is None:
+            identifier = 'Anonymous'
+        else:
+            identifier = obj.user.email
+
+        return identifier
+
+    def item_count(self, obj):
+        """ Return item count """
+        return obj.count()
+
+    # relabel admin list column headers
+    account_identifier.short_description = 'Account'
+    item_count.short_description = 'Items in Basket'
 
 
 admin.site.register(Basket, BasketAdmin)
