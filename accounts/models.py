7f149662f563730af32e9f8cc866d8cf9a536503
jyouung90ie <jyoung90ie@gmail.com>
2020-04-30 08:43:37 +0100

    Adds custom UserManager
    This was added to enable commandline creation of users and superusers.
    
    Migrations applied.

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,38 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """ Use email instead of username """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -15,8 +47,10 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = 'email'
 
-    REQUIRED_FIELDS = ['address', 'city', 'country', 'post_code',
-                       'date_of_birth']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'address', 'city',
+                       'country', 'post_code', 'date_of_birth']
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return f'{self.first_name} {self.last_name[:1]}'
